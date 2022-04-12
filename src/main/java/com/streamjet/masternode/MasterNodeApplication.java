6fc4961651236ffdfe8856b4c1b54c6472f4ce47
Cube bit <no_fucking_email_for_you@gmail.com>
2022-04-12 10:50:49 +0200

    spring initialization

---
+++ src/main/java/com/streamjet/masternode/MasterNodeApplication.java
@@ -0,0 +1,13 @@
+package com.streamjet.masternode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterNodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterNodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/streamjet/masternode/MasterNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamjet.masternode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterNodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
